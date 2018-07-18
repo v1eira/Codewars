6237548e1c14afdee04467f75b16de87dd17271c
Ewerson Vieira <ewersonv@gmail.com>
2018-07-18 12:35:41 -0300

    Create multiplies_of_3_or_5.py

---
+++ Python/multiplies_of_3_or_5.py
@@ -0,0 +1,18 @@
+#If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+#Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in.
+
+#Note: If the number is a multiple of both 3 and 5, only count it once.
+
+def solution(number):
+	sum = 0
+	i = 1
+	while i < number:
+		if i%3 == 0 and i%5 == 0:
+			sum += i
+		elif i%3 == 0:
+			sum += i
+		elif i%5 == 0:
+			sum += i
+		i += 1
+	return sum
