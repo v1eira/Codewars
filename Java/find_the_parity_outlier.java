deca4697f68e654e28f8f008ee2092a07b1c5ace
v1eira <ewersonv@gmail.com>
2019-07-19 03:03:01 -0300

    add Equal Sides Of An Array (Java)

--- Java/find_the_parity_outlier.java
+++ Java/find_the_parity_outlier.java
@@ -1,5 +1,7 @@
 /*
-You are given an array (which will have a length of at least 3, but could be very large) containing integers. The array is either entirely comprised of odd integers or entirely comprised of even integers except for a single integer N. Write a method that takes the array as an argument and returns this "outlier" N.
+You are given an array (which will have a length of at least 3, but could be very large) containing integers. The array
+is either entirely comprised of odd integers or entirely comprised of even integers except for a single integer N. Write
+a method that takes the array as an argument and returns this "outlier" N.
 
 Examples
 [2, 4, 0, 100, 4, 11, 2602, 36]
@@ -28,4 +30,5 @@ public class FindOutlier{
       return firstOdd;
     else
       return firstEven;
-  }}
+  }
+}
